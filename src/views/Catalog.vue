a51d2e877666a8efe72ae198ac07b328b91a094f
Maisan Auliya <103273340+MaeAuliya@users.noreply.github.com>
2022-12-17 22:06:27 +0700

    Update Catalog.vue

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,40 +1,37 @@
 <template>
-  <div class="home">
+  <div class="catalog" style="margin-top:10px;">
     <br />
-    <h1>/vue/ vueJS based forum site</h1>
+    <h1 class="title is-1" style="text-align:center; font-family: 'Covered By Your Grace', cursive; color:#1C98F7; font-size:70px">CATALOG</h1>
   </div>
-  <input type="text" v-model="search" name="search" placeholder="search thread">
-  <div class="row">
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body" v-for="forum in filteredItems" :key="forum.id">
-          <h5 class="card-title">{{ forum.Title }}</h5>
-          <div><img :src="forum.imageThread" /></div>
-          <p class="card-text">{{ forum.Description }}</p>
-          <router-link href="#" class="btn btn-primary" :to="{ path: `/thread/${forum.id}` }">view thread</router-link>
-          <br />
-          ----------------------------------------------------------------
+
+  <div class="columns is-centered" style="margin-top:40px; margin-bottom:30px">
+    <input class="input is-rounded" style="width:500px" type="text" v-model="search" name="search"
+      placeholder="search thread...">
+  </div>
+
+  <div class="box" style="position:absolute; left:300px;">
+    <div class="card" style="width:1000px; display:flex; margin-bottom:50px" v-for="forum in filteredItems"
+      :key="forum.id">
+      <div class="card-image is-rounded" style="align-items:center">
+        <img :src="forum.imageThread" onerror="this.style.display='none'"
+          style="width:200px;height:200px; object-fit: cover" />
+      </div>
+
+      <div class="card-content">
+        <h5>#{{ forum.id }}</h5>
+        <h5 class="title is-3" style="margin-top:20px;">{{ forum.Title }}</h5>
+        <div style="position:absolute;top:150px">
+          <router-link href="#" class="button is-info" :to="{ path: `/thread/${forum.id}` }">view thread</router-link>
         </div>
       </div>
     </div>
   </div>
 </template>
-<style>
-.card{
-   margin-top:20px;
-}
-.row {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
 
-.card img {
-  height: 200px;
-  width: 200px;
-  object-fit: cover;
-}
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Covered+By+Your+Grace&display=swap');
 </style>
+
 <script>
 // @ is an alias to /src
 import forColRef from "../firebase";
@@ -72,10 +69,10 @@ export default {
   created() {
     this.fetchData();
   },
-  computed: { 
-    filteredItems(){
+  computed: {
+    filteredItems() {
       return this.forums.filter((item) => {
-          return item.Title.match(this.search)
+        return item.Title.match(this.search)
       })
     }
   }

58e5cf4b1ac22f1fa3213e63187fddfb30291087
Maisan Auliya <103273340+MaeAuliya@users.noreply.github.com>
2022-12-17 22:07:10 +0700

    Update AddForum.vue

--- src/views/AddForum.vue
+++ src/views/AddForum.vue
@@ -1,27 +1,44 @@
 <template>
-  <div class="home">
-    <br/>
-    <h1>/vue/ vueJS based forum site</h1>
+  
+  <div class="home" style="margin-top:10px;margin-bottom:20px">
+    <br />
+    <h1 class="title is-1"
+      style="text-align:center; font-family: 'Covered By Your Grace', cursive; color:#1C98F7; font-size:70px">Add Thread
+    </h1>
   </div>
-  <form @submit.prevent="createForum">
-  <div class="form-group">
-    <label for="formGroupExampleInput">Title</label>
-    <input type="text" class="form-control"  placeholder="Title" v-model="dataInfo.Title">
-  </div>
-  <div class="form-group">
-    <label for="formGroupExampleInput2">Description</label>
-    <input type="text" class="form-control" placeholder="Description" v-model="dataInfo.Description">
-  </div>
-  <div class="mb-3">
-  <label for="formFile" class="form-label">Image</label>
-  <input class="form-control" type="file" id="formFile" @change="onFileChange">
-</div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+  <form class="box" @submit.prevent="createForum" style="width:1000px;position:absolute; left:350px">
+    <div class="field">
+      <label class="label">Title</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Title" v-model="dataInfo.Title">
+      </div>
+    </div>
+  
+    <div class="field">
+      <label class="label">Description</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Description Your Thread" v-model="dataInfo.Description">
+      </div>
+    </div>
+
+    <div class="field">
+      <label  class="label">Image</label>
+      <div class="file">
+        <label for="formFile"  class="file-label">
+          <input class="input" type="file" id="formFile" @change="onFileChange">
+        </label>
+      </div>
+    </div>
+  
+    <button class="button is-primary" style="margin-top:10px;">Submit</button>
+  </form>
 </template>
+
 
 <script>
 // @ is an alias to /src
+
 import forColRef from "../firebase";
 import { addDoc, serverTimestamp, getDoc, setDoc, doc, getCountFromServer, deleteDoc, collection } from "firebase/firestore";
 import db from "../firebase";
@@ -37,37 +54,38 @@ export default {
       threadInfo: {
         orderNow: null,
       },
-      dataInfo: { 
-      Title: '',
-      Description: '',
-      imageThread: null,
-      createdOrder: '',
-      replyAmount: null,
-      imageAmount: null,
-      prevThread: null,
-      imagepost: "",
-      bumpOrder: '',}
+      dataInfo: {
+        Title: '',
+        Description: '',
+        imageThread: null,
+        createdOrder: '',
+        replyAmount: null,
+        imageAmount: null,
+        prevThread: null,
+        imagepost: "",
+        bumpOrder: '',
+      }
     }
   },
   methods: {
     async createForum() {
       console.log("creating forum...");
-      this.dataInfo.imageThread=this.image;
-      this.dataInfo.createdOrder= serverTimestamp();
+      this.dataInfo.imageThread = this.image;
+      this.dataInfo.createdOrder = serverTimestamp();
       const addedDoc = await addDoc(forColRef, this.dataInfo);
       console.log()
-      this.addedDoc= addedDoc.id;
+      this.addedDoc = addedDoc.id;
       alert("forum added");
       this.$router.push("/");
     },
-     onFileChange(e) {
+    onFileChange(e) {
       var files = e.target.files || e.dataTransfer.files;
       if (!files.length)
         return;
       this.createImage(files[0]);
       console.log(this.image);
     },
-      createImage(file) {
+    createImage(file) {
       var image = new Image();
       var reader = new FileReader();
       var vm = this;
